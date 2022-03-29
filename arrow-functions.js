2e917c0c85c09e1ee47e9269311e943d99c739ca
wlillisdev <williamlillis100@gmail.com>
2022-03-29 19:49:54 +0000

    inital commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,30 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+let sum = addTwoNumbers(3,4);
+console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers4 = (a, b) => {
+    return a + b;
+}
+let sum2 = addTwoNumbers4 (3,4);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => a + b;
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething ("HELLO THERE");
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string
+    </p>`
+    )
+    console.log(returnMultipleLines());
